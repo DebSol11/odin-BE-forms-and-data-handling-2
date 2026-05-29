0f2626c56503e5273d2dcd36a7d1196a1d0c4c5e
Michael <100490812+DebSol11@users.noreply.github.com>
2026-05-29 11:32:00 +0200

    Eieieieieieieieieieeieieieieiei

--- server.js
+++ server.js
@@ -1,33 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+const usersRouter = require("./routes/usersRouter");
 
-// global actions need to be defined here because the execute in order
-app.use(logger);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use("/", usersRouter);
 
-app.get("/", (req, res) => {
-  console.log("Home Page");
-  res.send("Home Page");
-});
-
-app.get("/users", auth, (req, res) => {
-  console.log(`User is admin = ${req.admin}`);
-  console.log("Users Page");
-  res.send("Users Page");
-});
-
-// middleware definition
-function logger(req, res, next) {
-  console.log(req.originalUrl);
-  next();
-}
-
-function auth(req, res, next) {
-  if (req.query.admin === "true") {
-    req.admin = true;
-    next();
-    return;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
   }
-  res.send("No auth");
-}
-
-app.listen(3000);
+  console.log(`Express app listening on port ${PORT}!`);
+});
